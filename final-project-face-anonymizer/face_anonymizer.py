772fab2423e0a686af0331fcc767a692f6cf6ba1
tiagonuneslx <tiago.nunes.lx@gmail.com>
2024-01-28 22:36:47 +0000

    Final Project: - Completed face_anonymizer.py

--- final-project-face-anonymizer/face_anonymizer.py
+++ final-project-face-anonymizer/face_anonymizer.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from keras.src.utils.image_utils import ResizeMethod
 from matplotlib import pyplot as plt
 
+isTestMode = False
+
 image_files = [
     "resources/test/img1.jpg",
     "resources/test/img2.jpg",
@@ -65,8 +67,9 @@ for image_file in image_files:
 tf_images = tf.convert_to_tensor(images, dtype=tf.float32)
 images = tf_images
 
-visualize_detections(model, images, "xywh")
-plt.show()
+if isTestMode:
+    visualize_detections(model, images, "xywh")
+    plt.show()
 
 y_pred = model.predict(images)
 y_pred = keras_cv.bounding_box.to_ragged(y_pred)
@@ -87,10 +90,7 @@ for i, image_file in enumerate(image_files):
     elif img_width > img_height:
         y_adjustment = img_height / img_width
 
-    boxes_expanded = []
-
     for box in boxes:
-
         w, h = box[2], box[3]
 
         box = keras_cv.bounding_box.convert_format(
@@ -106,7 +106,7 @@ for i, image_file in enumerate(image_files):
         box[2] = box[2] * (img_width / 640)
         box[3] = box[3] * (img_height / 640)
 
-        scale_factor = .4
+        scale_factor = .1  # x1.2
 
         box[0] = max(0, box[0] - w * scale_factor)
         box[1] = max(0, box[1] - h * scale_factor)
@@ -117,11 +117,50 @@ for i, image_file in enumerate(image_files):
         end = (box[2], box[3])
         start = (np.rint(start)).astype(int)
         end = (np.rint(end)).astype(int)
-        cv2.rectangle(image, start, end, color=red, thickness=2)
+        if isTestMode:
+            cv2.rectangle(image, start, end, color=red, thickness=2)
 
-        boxes_expanded.append(box)
+        box = (np.rint(box)).astype(int)
 
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        roi = np.zeros_like(gray)
+        roi[box[1]:box[3], box[0]:box[2]] = 255
+        mask = cv2.bitwise_and(gray, gray, mask=roi)
 
+        (t, mask) = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU)
+
+        if isTestMode:
+            cv2.imshow(image_file, mask)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        # Closure
+        # Intended to close the gaps inside the coins
+        strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(w * .2), int(h * .2)))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, strel)
+
+        if isTestMode:
+            cv2.imshow("binary closed", mask)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        # Opening
+        # Intended to "break the coins away from each other"
+        strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(w * .7), int(h * .7)))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, strel)
+
+        if isTestMode:
+            cv2.imshow(image_file, mask)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        # mask = 255 - mask
+
+        # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        # image = cv2.bitwise_and(image, image, mask=mask)
+
+        blur = cv2.blur(image, (15, 25), 0)
+        image[mask > 0] = blur[mask > 0]
 
     cv2.imshow(image_file, image)
     cv2.waitKey(0)
